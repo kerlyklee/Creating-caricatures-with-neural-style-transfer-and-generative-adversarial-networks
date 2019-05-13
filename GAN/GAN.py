5ec214a5b4679bc8980689de19661b29f398bc92
kerlyklee <keklee@ttu.ee>
2019-05-13 23:51:03 +0300

    please

--- GAN/GAN.py
+++ GAN/GAN.py
@@ -13,7 +13,7 @@ slim = tf.contrib.slim
 
 HEIGHT, WIDTH, CHANNEL = 128, 128, 3
 BATCH_SIZE = 64
-EPOCH = 1500
+EPOCH = 2001
 version = 'new_caricatures11'
 newCaric_path = './' + version
 
@@ -177,6 +177,8 @@ def train():
     
     d_loss = tf.reduce_mean(real_result) - tf.log(1-fake_result)  # This optimizes the discriminator.
     g_loss = -tf.reduce_mean(tf.log(fake_result))  # This optimizes the generator.
+    d_loss = tf.convert_to_tensor(d_loss, dtype=tf.float32)
+    g_loss = tf.convert_to_tensor(g_loss, dtype=tf.float32)
             
     #d_loss = tf.reduce_mean(tf.nn.relu(1. - tf.log(1-real_result)))  # This optimizes the discriminator.
     #g_loss = -tf.reduce_mean(tf.nn.relu(1. +tf.log(fake_result)))  # This optimizes the generator.
