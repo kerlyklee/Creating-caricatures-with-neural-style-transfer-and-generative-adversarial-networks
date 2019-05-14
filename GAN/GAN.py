5b1efaab38832d639a57793b0aae73cc0ce66b3c
kerlyklee <keklee@ttu.ee>
2019-05-14 18:36:47 +0300

    even better ADAM

--- GAN/GAN.py
+++ GAN/GAN.py
@@ -13,8 +13,8 @@ slim = tf.contrib.slim
 
 HEIGHT, WIDTH, CHANNEL = 128, 128, 3
 BATCH_SIZE = 64
-EPOCH = 250
-version = 'new_caricatures12'
+EPOCH = 151
+version = 'new_caricatures14'
 newCaric_path = './' + version
 
 
@@ -186,9 +186,11 @@ def train():
     t_vars = tf.trainable_variables()
     d_vars = [var for var in t_vars if 'dis' in var.name]
     g_vars = [var for var in t_vars if 'gen' in var.name]
-    optimizer = tf.train.AdamOptimizer(learning_rate=1e-3, beta1=0.5)
-    trainer_d = optimizer.minimize(d_loss, var_list=d_vars)
-    trainer_g = optimizer.minimize(g_loss, var_list=g_vars)
+    #optimizer = tf.train.AdamOptimizer(learning_rate=1e-3, beta1=0.5)
+    #trainer_d = optimizer.minimize(d_loss, var_list=d_vars)
+    #trainer_g = optimizer.minimize(g_loss, var_list=g_vars)
+    trainer_d = tf.train.RMSPropOptimizer(learning_rate=2e-4).minimize(d_loss, var_list=d_vars)
+    trainer_g = tf.train.RMSPropOptimizer(learning_rate=2e-4).minimize(g_loss, var_list=g_vars)
     # clip discriminator weights
     d_clip = [v.assign(tf.clip_by_value(v, -0.01, 0.01)) for v in d_vars]
 
